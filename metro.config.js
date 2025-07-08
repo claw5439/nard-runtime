9751ae56040ffa9b796a5f55c53fc8a7cc5f873b
christopher lawrence <christopherlawrence@MacBookPro.hsd1.ct.comcast.net>
2025-07-07 22:04:49 -0400

    Try to use metro file store cache

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-
+// config.cac
+config.cacheStores = ({ FileStore }) => [
+    new FileStore({
+      root: './metro-cache'//path.join(os.tmpdir(), 'metro-cache'),
+    }),
+  ]
 module.exports = withNativeWind(config, { input: './global.css' });
