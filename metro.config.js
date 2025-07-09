7015139c3439877cd66aea360e19f5d140576c8a
christopher lawrence <christopherlawrence@MacBookPro.hsd1.ct.comcast.net>
2025-07-09 18:53:33 -0400

    big worker

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-// config.maxWorkers = 1;
+config.maxWorkers = 500;
 // config.cac
 config.transformer.minifierPath = 'metro-minify-esbuild';
 config.transformer.minifierConfig = {
