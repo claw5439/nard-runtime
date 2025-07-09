457d8b6f9ab785f487d4dd92f2826f31b2d8fa6e
christopher lawrence <christopherlawrence@MacBookPro.hsd1.ct.comcast.net>
2025-07-09 18:04:19 -0400

    Try es-build

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-config.maxWorkers = 1;
+// config.maxWorkers = 1;
 // config.cac
+config.transformer.minifierPath = 'metro-minify-esbuild';
+config.transformer.minifierConfig = {
+  // ESBuild options...
+};
+
 config.cacheStores = ({ FileStore }) => [
     new FileStore({
       root: './metro-cache'//path.join(os.tmpdir(), 'metro-cache'),
